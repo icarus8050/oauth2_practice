bb2bba3e4184159ba724810e74ba6df38bf21fe7
icarus8050 <icarus8050@naver.com>
2020-01-20 23:30:45 +0900

    Initial commit

---
+++ src/main/java/com/icarus/oauth2_practice/Oauth2PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.icarus.oauth2_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2PracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icarus/oauth2_practice/Oauth2PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icarus.oauth2_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
