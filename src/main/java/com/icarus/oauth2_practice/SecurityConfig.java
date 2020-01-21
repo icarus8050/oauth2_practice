e12eecd23bbc0b2dd3921c8407fb78cce49c5609
icarus8050 <icarus8050@naver.com>
2020-01-21 23:45:10 +0900

    OAuth Simple flow page

--- src/main/java/com/icarus/oauth2_practice/SecurityConfig.java
+++ src/main/java/com/icarus/oauth2_practice/SecurityConfig.java
@@ -12,8 +12,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/", "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .oauth2Login();
+                .oauth2Login()
+                .loginPage("/login");
     }
 }
